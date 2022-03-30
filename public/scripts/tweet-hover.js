f930ab084e8ae251c76de5d5560a47d6976ee075
Isaiah <isaiah.iu@hotmail.com>
2022-03-30 01:54:38 +0000

    Enable box-shadow on tweet post, icons WIP

--- public/scripts/tweet-hover.js
+++ public/scripts/tweet-hover.js
@@ -1,1 +1,17 @@
-$(document).ready(function () {});
+$(document).ready(function () {
+	$(".tweet")
+		.on("mouseenter", function () {
+			$(this).addClass("box-hover");
+		})
+		.on("mouseleave", function () {
+			$(this).removeClass("box-hover");
+		});
+
+	$(".tweet span > i")
+		.on("mouseenter", function () {
+			$(this).addClass("gold");
+		})
+		.on("mouseleave", function () {
+			$(this).removeClass("gold");
+		});
+});
