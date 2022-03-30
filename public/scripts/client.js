a648726a2ed6fcf667b2edcaaacb614e46f15358
Isaiah <isaiah.iu@hotmail.com>
2022-03-30 18:07:02 +0000

    Create renderTweets function in client.js
    - Change test/drive code for tweetData to hold array
    of objects.
    - Test out functionality of renderTweets, will now
    take array of objects and render into html.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,20 +5,33 @@
  */
 
 // Test / driver code (temporary). Eventually will get this from the server.
-const tweetData = {
-	user: {
-		name: "Newton",
-		avatars: "https://i.imgur.com/73hZDYK.png",
-		handle: "@SirIsaac",
+const tweetData = [
+	{
+		user: {
+			name: "Newton",
+			avatars: "https://i.imgur.com/73hZDYK.png",
+			handle: "@SirIsaac",
+		},
+		content: {
+			text: "If I have seen further it is by standing on the shoulders of giants",
+		},
+		created_at: 1648489672164,
 	},
-	content: {
-		text: "If I have seen further it is by standing on the shoulders of giants",
+	{
+		user: {
+			name: "Descartes",
+			avatars: "https://i.imgur.com/nlhLi3I.png",
+			handle: "@rd",
+		},
+		content: {
+			text: "Je pense , donc je suis",
+		},
+		created_at: 1648576072165,
 	},
-	created_at: 1461116232227,
-};
+];
 
 function createTweetElement(tweet) {
-	const tweetElement = `			
+	const $tweet = `			
     <article class="tweet">
 			<header>
 			  <span>
@@ -38,13 +51,17 @@ function createTweetElement(tweet) {
 				</span>
 			</footer>
 		</article>`;
-	return tweetElement;
+	return $tweet;
 }
 
-const $tweet = createTweetElement(tweetData);
+function renderTweets(arr) {
+	for (let tweet of arr) {
+		let $post = createTweetElement(tweet);
+		$(".container").append($post);
+	}
+}
 
 // Test / driver code (temporary)
 $(() => {
-	$(".container").append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
-	console.log($tweet); // to see what it looks like
+	renderTweets(tweetData);
 });
